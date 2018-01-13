b7672c3d5a6fea1a927bf78db23456ffc472de5d
Casper Biever <c.biever@gmail.com>
2018-01-13 13:22:21 +0100

    Added CSV export, working on PDF.

--- app/components/start-list-header.js
+++ app/components/start-list-header.js
@@ -21,16 +21,43 @@ export default Component.extend({
       this.navigate(this.get('location'), this.get('date'), 'ktrax');
     },
     downloadPDF() {
-
+      this.download('application/pdf');
     },
     downloadCSV() {
-
+      this.download('text/csv');
     }
   },
   navigate: function(location, date, route) {
     if (!route) {
       route = this.get('router').get('currentRouteName');
     }
-    this.get('router').transitionTo(route, location.get('name').toLowerCase(), date.getFullYear() + '-' + (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1) + '-' + (date.getDate() < 10 ? '0' : '') + date.getDate());
+    this.get('router').transitionTo(route, location.get('name'), this.formatDate(date));
+  },
+  download: function(mimeType) {
+    let filenameExtension = mimeType.split('/')[1];
+    let filename = this.downloadFilename(filenameExtension);
+    let url = '/api/v1/rs/flights?format=' + filenameExtension;
+    if (this.get('location')) {
+      url += '&filter[location]=' + this.get('location').get('name');
+    }
+    if (this.get('date')) {
+      url += '&filter[date]=' + this.formatDate(this.get('date'));
+    }
+    Ember.$.get(url).then(function(data) {
+      window.saveAs(new Blob([data], {type: mimeType}), filename);
+    });
+  },
+  formatDate(date, separator = '-') {
+    return date.getFullYear() + separator + (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1) + separator + (date.getDate() < 10 ? '0' : '') + date.getDate();
+  },
+  downloadFilename(filenameExtension) {
+    let filename = 'flights';
+    if (this.get('date')) {
+      filename = this.formatDate(this.get('date'), '');
+    }
+    if (this.get('location')) {
+      filename = (this.get('date') ? filename + ' ' : '') + this.get('location').get('name');
+    }
+    return filename + '.' + filenameExtension;
   }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,5 +21,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/file-saver/FileSaver.min.js');
+
   return app.toTree();
+
 };
